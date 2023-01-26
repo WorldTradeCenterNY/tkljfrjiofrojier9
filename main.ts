56a6468544e729482409419807277840ebfdb061
WorldTradeCenterNY <122543666+WorldTradeCenterNY@users.noreply.github.com>
2023-01-26 09:50:48 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+OLED.init(128, 64)
+OLED.writeStringNewLine("worldtradecenter")
+OLED.writeStringNewLine("\"log outta ur macs boies")
+OLED.writeStringNewLine("this is connor Higgins btw")
+OLED.writeStringNewLine("good morning usa")
+OLED.writeStringNewLine("ah just as the founding fathers intended")
+basic.forever(function () {
+	
+})
